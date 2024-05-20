88f6240358fea6f592c531ba8aee69118f36daf8
kako05 <banana_manbou@yahoo.co.jp>
2024-05-20 14:40:38 +0900

    filmコントローラーの作成

---
+++ spec/requests/films_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Films", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/films/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
