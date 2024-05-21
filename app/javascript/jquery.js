bbc1b4b3a17fe29ad7a4aef87408c28d026435cf
kako05 <banana_manbou@yahoo.co.jp>
2024-05-21 16:55:57 +0900

    J.S設定

---
+++ app/javascript/jquery.js
@@ -0,0 +1,2 @@
+// jquery.js
+document.write('<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"><\/script>');
