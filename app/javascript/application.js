bd5d4c30dc29130c9fb6a5c0bc968b114091ead8
kako05 <banana_manbou@yahoo.co.jp>
2024-05-23 16:52:18 +0900

    gem削除に伴う記述の削除

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
 import "@hotwired/turbo-rails"
 import "controllers"
