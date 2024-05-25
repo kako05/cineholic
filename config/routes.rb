50aa333a9fca8d451b305412ab075a166dd2fdfa
kako05 <banana_manbou@yahoo.co.jp>
2024-05-26 01:07:31 +0900

    ルーテイングとコントローラーの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'films#index'
+
+  root "films#index"
+
   resources :films do
     collection do
       get "showing", to: "films#showing", as: :showing_films
       get "search", to: "films#search", as: :search_films
     end
-    member do
-      get "show_details", to: "films#show_details"
-    end
   end
-  get "films/search", to: "films#search", as: :search_films
 
   resources :users, only: [:show, :edit, :update]
 end
