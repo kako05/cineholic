1a1b4c9b9765e7c1aab66fa3426cd59c9303fa5d
kako05 <banana_manbou@yahoo.co.jp>
2024-05-28 23:36:48 +0900

    unicornの訂正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,7 +8,7 @@ worker_processes 1
 working_directory "#{app_path}/current"  # 「current」を指定
 
 #Unicornの起動に必要なファイルの設置場所を指定
-working_directory "#{app_path}/current"  # 「current」を指定
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"  # 「shared」の中を参照するよう変更
 
 #ポート番号を指定
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"  # 「shared」の中を参照するよう変更
