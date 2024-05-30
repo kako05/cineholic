fa3635f5307f5c42cc6434590016df3a5e2a313a
kako05 <banana_manbou@yahoo.co.jp>
2024-05-30 23:27:22 +0900

    検索機能/コントローラー編集

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -4,10 +4,10 @@ class FilmsController < ApplicationController
 
   def index
     @films = if params[:q].present? || search_params_present?
-      search_films.page(params[:page]).per(10)
-    else
-      Film.all.page(params[:page]).per(10)
-    end
+               search_films.page(params[:page]).per(10)
+             else
+               Film.all.page(params[:page]).per(10)
+             end
   end
 
   def showing
@@ -26,51 +26,54 @@ class FilmsController < ApplicationController
 
   def search_films
     films = Film.all
-  
+
     if params[:q_cont].present?
       keyword = normalize_search_term(params[:q_cont])
-  
+
       films = films.joins("LEFT JOIN film_casts ON films.id = film_casts.film_id")
                    .joins("LEFT JOIN casts ON film_casts.cast_id = casts.id")
                    .joins("LEFT JOIN film_trailers ON films.id = film_trailers.film_id")
                    .joins("LEFT JOIN trailers ON film_trailers.trailer_id = trailers.id")
-  
+
       # 検索条件を設定する
       conditions = []
+      binding.pry
 
       # タイトルの検索条件
       conditions << "films.title LIKE :q" if params[:search_title].present?
-  
+
       # 役者名の検索条件
       conditions << "casts.name LIKE :q" if params[:search_cast].present?
-  
+
       # スタッフ名の検索条件
       conditions << "trailers.name LIKE :q" if params[:search_staff].present?
-  
+
       # キーワードと条件を結合
-      query = conditions.join(" OR ")
-  
-      # 実行
-      films = films.where(query, q: keyword.map { |k| "%#{k}%" })
+      unless conditions.empty?
+        query = conditions.join(" OR ")
+
+        # 実行
+        films = films.where(query, q: "%#{keyword}%")
+      end
     end
-  
+
     # 重複を除去
     films = films.distinct
-  
+
     # 映画が見つからない場合の処理
     if films.empty?
       flash.now[:alert] = '映画が見つかりません'
     end
-  
+
     films
   end
 
   def normalize_search_term(term)
-    term_kanji = term.tr('a-zA-Z', 'ａ-ｚＡ-Ｚ')
+    term_kanji = NKF.nkf('-w -Z1', term)
     term_kana = NKF.nkf('-w -Z1 -x', term)
     term_hira = NKF.nkf('-w -Z1 -X', term)
 
-    ["%#{term_kanji}%", "%#{term_kana}%", "%#{term_hira}%"]
+    [term_kanji, term_kana, term_hira]
   end
 
   def search_params_present?
