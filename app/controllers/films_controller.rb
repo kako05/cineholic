029051157c3bed044318eff267de7d72cf048b05
kako05 <banana_manbou@yahoo.co.jp>
2024-05-31 14:01:53 +0900

    検索機能追加

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -26,57 +26,63 @@ class FilmsController < ApplicationController
 
   def search_films
     films = Film.all
-
+  
     if params[:q_cont].present?
-      keyword = normalize_search_term(params[:q_cont])
-
+      keywords = normalize_search_term(params[:q_cont]).split(' ')
       films = films.joins("LEFT JOIN film_casts ON films.id = film_casts.film_id")
                    .joins("LEFT JOIN casts ON film_casts.cast_id = casts.id")
                    .joins("LEFT JOIN film_trailers ON films.id = film_trailers.film_id")
                    .joins("LEFT JOIN trailers ON film_trailers.trailer_id = trailers.id")
-
+  
       # 検索条件を設定する
       conditions = []
-      binding.pry
-
-      # タイトルの検索条件
-      conditions << "films.title LIKE :q" if params[:search_title].present?
-
-      # 役者名の検索条件
-      conditions << "casts.name LIKE :q" if params[:search_cast].present?
-
-      # スタッフ名の検索条件
-      conditions << "trailers.name LIKE :q" if params[:search_staff].present?
-
-      # キーワードと条件を結合
-      unless conditions.empty?
-        query = conditions.join(" OR ")
-
-        # 実行
-        films = films.where(query, q: "%#{keyword}%")
+  
+      # タイトルがtrue
+      conditions << "LOWER(films.title) LIKE :q" if params[:search_title] == "1"
+  
+      # 役者名がtrue
+      conditions << "LOWER(casts.name) LIKE :q" if params[:search_cast] == "1"
+  
+      # スタッフ名がtrue
+      conditions << "LOWER(trailers.name) LIKE :q" if params[:search_staff] == "1"
+  
+      # チェックボックスがすべてfalseのときdescriptionとtextカラムを除外して検索
+      if conditions.empty?
+        columns_to_search = (Film.column_names - ["description"]) + (Trailer.column_names - ["text"])
+        columns_to_search.each do |column|
+          conditions << "LOWER(films.#{column}) LIKE :q"
+        end
       end
+  
+      # 各キーワードに対して部分一致検索を行う
+      keyword_conditions = []
+      conditions.each do |condition|
+        keyword_conditions << keywords.map { |keyword| "#{condition}" }
+      end
+  
+      # キーワードと条件を結合
+      query = keyword_conditions.flatten.join(" OR ")
+      films = films.where(query, q: keywords.map { |keyword| "%#{keyword}%" })
+  
+      films = films.distinct
     end
-
-    # 重複を除去
-    films = films.distinct
-
+  
     # 映画が見つからない場合の処理
     if films.empty?
       flash.now[:alert] = '映画が見つかりません'
     end
-
+  
     films
   end
+  
 
   def normalize_search_term(term)
-    term_kanji = NKF.nkf('-w -Z1', term)
-    term_kana = NKF.nkf('-w -Z1 -x', term)
-    term_hira = NKF.nkf('-w -Z1 -X', term)
-
-    [term_kanji, term_kana, term_hira]
+    term = NKF.nkf('-w -Z1', term) # 入力文字列を半角に変換
+    term.downcase! # 入力文字列を小文字に変換
+    term.gsub!(/\s+/, '%') # 空白を '%' に変換して部分一致検索に対応
+    "%#{term}%"
   end
 
   def search_params_present?
     params.values_at(:search_title, :search_cast, :search_staff).any?(&:present?)
   end
-end
