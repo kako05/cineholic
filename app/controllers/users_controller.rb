bbd070d58edfe788d638c49ec37293cd6ca46a0e
kako05 <banana_manbou@yahoo.co.jp>
2024-06-04 00:39:29 +0900

    マイページのコメント一覧

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def show
-    @user = User.find(params[:id])
+    @user = current_user
+    @comments = @user.comments.includes(:film)
   end
 
   def edit
